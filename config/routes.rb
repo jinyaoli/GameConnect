be3803540f0da5c87ce3898f9803099f0d0c0340
Jin Li <henryli123456@hotmail.com>
2017-04-25 15:22:20 +0000

    week3

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
     get 'contact-us', to: 'contacts#new', as: 'new_contact'
+    devise_for :users, controllers: { registrations: 'users/registrations' }
 end
